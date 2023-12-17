42d7a51380312389e77a0da26cbe1826a3ce44e8
kthwaite <28732700+kthwaite@users.noreply.github.com>
2023-12-17 14:25:14 +0000

    chore: Remove example

--- home.nix
+++ home.nix
@@ -62,13 +62,6 @@
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
